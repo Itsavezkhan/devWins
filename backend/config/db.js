c3891d1b9928969d0da1cdee62375df05fd00448
Avez khan <126850111+Itsavezkhan@users.noreply.github.com>
2025-10-26 16:21:31 +0530

    url name changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URL = process.env.URL;
+const MONGO_URL = process.env.MONGO_URL ;
 
 const db = async () => {
   try {
